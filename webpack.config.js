7fa1c5405711cbc22a73e00635aff2e71405b3c5
Corneliu <29034380+mishimomei@users.noreply.github.com>
2023-11-14 09:46:45 +0200

    added boilerplate setup, webpack config, gitignore config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      title: 'TOP Battleship',
+      filename: 'index.html',
+      inject: 'head',
+      scriptingLoading: 'defer',
+    })
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
